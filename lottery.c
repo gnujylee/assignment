0d0841a45caa2f4aae37522da369025c5cac6b91
gnujylee <49223831+gnujylee@users.noreply.github.com>
2019-06-05 12:03:20 +0900

    Update lottery.c
    번호 생성을 여러개 기능추가

--- lottery.c
+++ lottery.c
@@ -5,10 +5,14 @@
 #include <time.h>
 main() {
 	/* 변수선언 */
-	int ctr, i; //반복문에 쓰일 변수들
+	int ctr, i, j; //반복문에 쓰일 변수들
 	int lotto[6] = { 0, }; //로또번호가 저장될 정수형배열 모두 0으로 초기화
 	srand((unsigned int)time(NULL)); //난수생성기의 초기값결정
 	int number; //랜덤함수를 통해 로또번호를 임시로 저장할 변수
+	int n1; // 반복할 갯수
+	printf("반복할 숫자를 입력하시오:");
+	scanf("%d", &n1);
+	for(j=0; j< n1; j++){
 
        /*로또번호 자동 랜덤추출 */
 	for (i = 0; i<6; i++) { //총 6번 추첨
@@ -20,10 +24,12 @@ main() {
 			}
 		}
 	}
+	
 	/*랜덤 로또번호 저장 배열 출력*/
 	printf("\t로또번호\t");
 	for (i = 0; i < 6; i++) {
 		printf("%d\t", lotto[i]);
+	}
 	}
 	printf("\n");
 	system("pause");
