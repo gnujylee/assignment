70404a77dcc4b4f2fa3218b8b6df8af8ccc4fead
gnujylee <49223831+gnujylee@users.noreply.github.com>
2019-06-12 09:44:58 +0900

    Update lottery.c
    시뮬레이션 구현 temp

--- lottery.c
+++ lottery.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int main() {
 	/* 변수선언 */
@@ -11,7 +12,7 @@ int main() {
 	srand((unsigned int)time(NULL)); //난수생성기의 초기값결정
 	int m;
 	int n1; // 반복할 갯수 저장할 변수
-	
+	int count=0;
 	
 	
 	/*메뉴선택 */
@@ -25,9 +26,8 @@ int main() {
 			{
 				printf("몇줄을 출력할것인지를 입력하시오:");
 				scanf("%d", &n1);
+                                /*로또번호 자동 랜덤추출 */
 				for(j=0; j< n1; j++){
-
-       				/*로또번호 자동 랜덤추출 */
 				for (i = 0; i<6; i++) { //총 6번 추첨
 					lotto[i] = (rand() % 45) + 1;  // 1번부터 45번까지 로또번호 추첨
 					for (ctr = 0; ctr < i; ctr++) {   //중복되는 번호가 있는지 배열탐색
@@ -52,7 +52,33 @@ int main() {
 		
 		case '2':
 			{
-			printf("시뮬레이션 메뉴 구현예정\n");
+			printf("시뮬레이션\n");
+                        printf("로또 몇장을 구매할까요?(1장=5,000원)\n");
+                        scanf("%d", &count);
+/*
+                        
+  				for(j=0; j< n1; j++){
+
+       				/*로또번호 자동 랜덤추출 */
+				for (i = 0; i<6; i++) { //총 6번 추첨
+					lotto[i] = (rand() % 45) + 1;  // 1번부터 45번까지 로또번호 추첨
+					for (ctr = 0; ctr < i; ctr++) {   //중복되는 번호가 있는지 배열탐색
+						if (lotto[i] == lotto[ctr]) {
+						lotto[i] = (rand() % 45) + 1;
+						i--;
+						}
+					}
+				}
+
+				printf("\t로또번호\t");
+				for (i = 0; i < 6; i++) {
+					printf("%d\t", lotto[i]);
+				}
+				printf("\n");
+				}
+				printf("\n");
+*/
+			printf("%d \n", count);
 			break;
 			}
 		default:
@@ -65,3 +91,4 @@ int main() {
 	
 
 }
+
